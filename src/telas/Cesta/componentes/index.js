1f109f4ec0c8fd923ef0cfd37e601f6b987a04fd
anery <anery@magnasistemas.com.br>
2022-01-25 21:42:11 -0300

    Subindo projeto react-native

---
+++ src/telas/Cesta/componentes/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+
+import Topo from './Topo';
+import Detalhes from './Detalhes';
+
+export default function Cesta({topo, detalhes}){
+return <>
+    <Topo{...topo}/>
+
+    <View style={estilos.cesta}>
+        <Detalhes{...detalhes}/>
+</View>
+
+    </>
+}
+
+const estilos = StyleSheet.create({
+
+    cesta:{
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+   
+});
